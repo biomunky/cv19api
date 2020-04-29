5220804dc60aced2f908c3a66b77254169450e3f
Dan Klose <dan.klose@protonmail.com>
2020-04-29 15:11:09 +0100

    Add Parameterised Queries
    The api allows two start and end points to be set for all endpoints:
    
    * from / to
    * recordedOnFrom / recordedOnTo
    
    These are now exposed as two sets of functions, one parameterised the other not.
    In order to switch between from/recordedOnFrom the use has to set a bool called
    use_ground_truth.
    
    Bump version number

--- examples/get_deaths_by_region.rs
+++ examples/get_deaths_by_region.rs
@@ -1,10 +1,17 @@
-use covid19;
+use chrono::NaiveDate;
 
 fn main() {
-    let deaths_by_region = covid19::api::deaths_by_region();
+    let deaths = covid19::api::deaths_by_region();
 
-    if let Ok(resp) = deaths_by_region {
+    if let Ok(resp) = deaths {
         println!("{:?}", resp.data[0]);
         println!("{:?}", resp.metadata)
     }
+
+    let from = NaiveDate::from_ymd(2020, 4, 15);
+    let to = NaiveDate::from_ymd(2020, 4, 25);
+    let use_ground_truth = true;
+    let parameterised = covid19::api::parameterised_deaths_by_region(from, to, use_ground_truth);
+
+    println!("{:?}", parameterised)
 }
